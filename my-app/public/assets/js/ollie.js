f2f5af11f3f770624d928492628a2b961028bfa1
ServicioRivera <ale.mayen.100@gmail.com>
2022-08-30 19:53:02 -0600

    ServiciosRivera

---
+++ my-app/public/assets/js/ollie.js
@@ -0,0 +1,80 @@
+/*!
+=========================================================
+* Ollie Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// portfolio carousel
+$('#owl-portfolio').owlCarousel({
+    margin:30,
+    dots: false,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:false,
+            loop:false
+        }
+    }
+});
+
+// testmonial carousel
+$('#owl-testmonial').owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    nav: true,
+    dots: false
+})
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Leer mas";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Leer menos";
+      moreText.style.display = "inline";
+    }
+  }
